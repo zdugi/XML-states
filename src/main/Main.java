2a3631bd3c9ae910f446f84ff74fb2412b791f08
mikecitt <45363165+mikecitt@users.noreply.github.com>
2019-06-27 17:15:30 +0200

    mikec je car

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,6 @@ package main;
 public class Main {
     public static void main(String[] argv)
     {
-        System.out.println("Hello World.");
+        System.out.println("Hello World. Mikec je najbolji");
     }
 }
