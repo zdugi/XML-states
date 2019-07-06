5b8a28e4b3c71f5e100cd67547fa3e96a9689f19
mikecitt <45363165+mikecitt@users.noreply.github.com>
2019-07-06 19:31:50 +0200

    MainFrame GUI without functionality.

--- src/main/MainFrame.java
+++ src/main/MainFrame.java
@@ -1,13 +1,32 @@
 package main;
 
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 
 public class MainFrame extends JFrame {
-    public MainFrame() {
+    private MainFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("XML-states");
         setSize(800, 600);
         setLocationRelativeTo(null);
+
+        JMenuBar menuBar;
+        JMenu menu;
+        JMenuItem load, close;
+
+        menuBar = new JMenuBar();
+        menu = new JMenu("File");
+        load = new JMenuItem("Load");
+        load.setMnemonic(KeyEvent.VK_L);
+        close = new JMenuItem("Close");
+        close.setMnemonic(KeyEvent.VK_C);
+
+        menuBar.add(menu);
+        menu.add(load);
+        menu.addSeparator();
+        menu.add(close);
+        setJMenuBar(menuBar);
+
     }
     public static void main(String[] argv)
     {
