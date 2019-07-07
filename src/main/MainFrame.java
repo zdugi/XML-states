b2cee2ee90b037c2a91436755bd5e2a571439e89
mikecitt <45363165+mikecitt@users.noreply.github.com>
2019-07-07 20:14:14 +0200

    Loading Document, Close App...

--- src/main/MainFrame.java
+++ src/main/MainFrame.java
@@ -4,17 +4,19 @@ import tools.XMLReader;
 import view.DocumentView;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 
 public class MainFrame extends JFrame {
     private DocumentView documentView;
     private String documentPath;
+
     public MainFrame() {
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         setTitle("XML-states");
         setSize(600, 400);
-        setMinimumSize(new Dimension(400, 200));
+        setMinimumSize(new Dimension(450, 250));
         setLocationRelativeTo(null);
 
         JMenuBar menuBar;
@@ -24,9 +26,52 @@ public class MainFrame extends JFrame {
         menuBar = new JMenuBar();
         menu = new JMenu("File");
         load = new JMenuItem("Load");
-        load.setMnemonic(KeyEvent.VK_L);
+        load.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser j = new JFileChooser();
+                j.setAcceptAllFileFilterUsed(false);
+                j.setDialogTitle("Select a .xml file");
+                FileNameExtensionFilter restrict = new FileNameExtensionFilter("XML document file (*.xml)", "xml");
+                j.addChoosableFileFilter(restrict);
+                int retVal = j.showOpenDialog(null);
+                if(retVal == JFileChooser.APPROVE_OPTION)
+                {
+                    documentPath = j.getSelectedFile().getAbsolutePath();
+                }
+            }
+        });
         close = new JMenuItem("Close");
-        close.setMnemonic(KeyEvent.VK_C);
+        close.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int result = JOptionPane.showConfirmDialog(null, "Are you sure? ", "Confirm", JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+
+                if(result == JOptionPane.YES_OPTION){
+                    System.exit(0);
+                }else{
+                    //Do nothing
+                }
+            }
+        });
+
+        //close.addActionListener();
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int result = JOptionPane.showConfirmDialog(null, "Are you sure? ", "Confirm", JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+
+                if(result == JOptionPane.YES_OPTION){
+                    System.exit(0);
+                }else{
+                    //Do nothing
+                }
+            }
+        });
+
+
+
 
         menuBar.add(menu);
         menu.add(load);
