574efa5b3d94b59c3b960fe0ba50f43e7f196c4a
milanjokanovic <milan.jokanovic98@gmail.com>
2019-07-12 22:42:24 +0200

    DodajState view kreiran

--- src/gui/mainWindows/StateTransitonWindow.java
+++ src/gui/mainWindows/StateTransitonWindow.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.DefaultComboBoxModel;
@@ -28,6 +29,7 @@ import javax.swing.table.DefaultTableModel;
 import enums.TipStanja;
 import gui.popUps.EditComp;
 import gui.popUps.NewCompChoice;
+import main.MainTest;
 import model.komponente.Komponenta;
 
 public class StateTransitonWindow extends JFrame {
@@ -195,34 +197,48 @@ public class StateTransitonWindow extends JFrame {
 			AddState();
 			return;
 		}
-		else if(result == 1)
+		else if(result == 2)
 			return;
+		System.out.println(result);
 		String []podaciAkcije = {"Enabled", "Disabled"};
-		JComponent[] inputs2 = new JComponent[10];
-		inputs2[0] = new JLabel("INIT_STATE");
+		JComponent[] inputs2 = new JComponent[8];
+		inputs2[0] = new JLabel("Save");
 		JComboBox<String> cbA1 = new JComboBox<String>();
 		cbA1.setModel(new DefaultComboBoxModel<String>(podaciAkcije));
 		inputs2[1] = cbA1;
-		inputs2[2] = new JLabel("SAVED_STATE");
+		inputs2[2] = new JLabel("Submit");
 		JComboBox<String> cbA2 = new JComboBox<String>();
 		cbA2.setModel(new DefaultComboBoxModel<String>(podaciAkcije));
 		inputs2[3] = cbA2;
-		inputs2[4] = new JLabel("SUBMITED_STATE");
+		inputs2[4] = new JLabel("Reject");
 		JComboBox<String> cbA3 = new JComboBox<String>();
 		cbA3.setModel(new DefaultComboBoxModel<String>(podaciAkcije));
 		inputs2[5] = cbA3;
-		inputs2[6] = new JLabel("REJECTED_STATE");
+		inputs2[6] = new JLabel("Archive");
 		JComboBox<String> cbA4 = new JComboBox<String>();
 		cbA4.setModel(new DefaultComboBoxModel<String>(podaciAkcije));
 		inputs2[7] = cbA4;
-		inputs2[8] = new JLabel("ARCHIVED_STATE");
-		JComboBox<String> cbA5 = new JComboBox<String>();
-		cbA5.setModel(new DefaultComboBoxModel<String>(podaciAkcije));
-		inputs2[9] = cbA5;
+		
 		
 		result = JOptionPane.showOptionDialog(null, inputs2, "Moguce akcije", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE,null, null,null);
-		if(result == 0)
+		if(result == 2)
 			return;
+		String []podaciKomponente = {"Mandatory", "Delete", "Hide"};
+		ArrayList<Komponenta> lista = MainTest.data.getDokument().getKomponente();
+		int n = 2 * lista.size();
+		System.out.println(n);
+		JComponent[] inputs3 = new JComponent[n];
+		for(int i = 0, j = 0; i + j < n; i ++, j++)
+		{
+			inputs3[i + j] = new JLabel(lista.get(i).getNaziv());
+			inputs3[i + j + 1] = new JComboBox<String>(new DefaultComboBoxModel<String>(podaciKomponente));
+		}
+		result = JOptionPane.showOptionDialog(null, inputs3, "Stanje Komponenti", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE,null, null,null);
+		if(result == 2)
+			return;
+		String id =  tf.getText() + cb.getSelectedItem();
+		String []pod = {id, tf.getText(), cb.getSelectedItem().toString()};
+		dmS.addRow(pod);
 	}
 	
 	private void AddTransiton()
